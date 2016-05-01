f67c6db23e4581b6abbcc45d7214056fc0f82f2f
evyatarc <evyatarc@gmail.com>
2016-04-30 22:55:52 -0400

    Moving things around

--- tamid_matching.py
+++ tamid_matching.py
@@ -1,8 +1,3 @@
-
-"""
-Download Munkres from http://software.clapper.org/munkres/#installing_via_easyinstall
-Download Anaconda PYTHON version 2.7 from https://www.continuum.io/downloads
-"""
 import sys
 from munkres import Munkres, print_matrix, make_cost_matrix
 import pandas as pd
@@ -20,14 +15,6 @@ for row, column in indexes:
     total += value
     column_name = df.columns[column]  #new addition Ev 4.30.16
     school_name = df.index[row]		#new addition Ev 4.30.16
-    print column_name	#new addition Ev 4.30.16
-    print school_name	#new addition Ev 4.30.16
+    print "\n%s, %s" % (column_name, school_name)#new addition Ev 4.30.16
     print '(%d, %d) -> %d' % (row, column, value)
 print 'total profit=%d' % total
-
-"""
-Sources
-https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/utils/linear_assignment_.py
-https://github.com/scipy/scipy/blob/v0.17.0/scipy/optimize/_hungarian.py#L13-L107
-http://docs.scipy.org/doc/scipy-0.17.0/reference/generated/scipy.optimize.linear_sum_assignment.html
-"""
---
+++ thoughts.py
@@ -0,0 +1,7 @@
+"""
+Additional Notes:
+http://software.clapper.org/munkres/#installing_via_easyinstall
+https://github.com/scikit-learn/scikit-learn/blob/master/sklearn/utils/linear_assignment_.py
+https://github.com/scipy/scipy/blob/v0.17.0/scipy/optimize/_hungarian.py#L13-L107
+http://docs.scipy.org/doc/scipy-0.17.0/reference/generated/scipy.optimize.linear_sum_assignment.html
+"""
